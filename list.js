a273b30a18170f7c44e534bfeb27e39cb650a746
Jenny Tang <jennydtang@gmail.com>
2020-04-07 16:56:14 -0400

    used jQuery for toDoList

--- list.js
+++ list.js
@@ -1,19 +1,24 @@
 
-function todoList(){
-    var item = document.getElementById("todoInput").value //connects to input in form
-    var text = document.createTextNode(item) //get the value(item) and enter the value into text.
-    //TextNode objects make it possible to insert texts into the document as nodes (appendChild, insertBefore)
-    var newItem = document.createElement("li") //creates li item
-    newItem.appendChild(text) //take text entered and put into li
-    document.getElementById("todoList").appendChild(newItem) //grab list and add li to it
-    todoInput.value = ''; //clears input box
-
-
-}
-document.querySelector("ul").addEventListener('click', function(done){ //add event listener to list
-    var doneItem =done.target; //target of item for event
-    if(doneItem.tagName.toUpperCase()=="LI"){ //declare target(doneItem) to equal li
-        doneItem.parentNode.removeChild(doneItem); //now that li equals target, remove the child of UL, which is the li
-    }
+//remove a list item
+//grab the list, and apply function on click. add "on" to apply to new list items 
+$("ul").on("click", "li", function(){ 
+    $(this).css("text-decoration", "line-through"); //add style to item
+        setTimeout(() => {
+        $(this).remove(); //clears it
+        }, 1000)
 });
-    
+
+//add function to add button
+$('#todoBtn').click(function(){
+//take input and make it into a list item
+    $('#list').append('<li>' + $('#newItem').val() + '</li>');
+    $('#newItem').val("");    
+  })
+
+
+
+
+
+
+
+
